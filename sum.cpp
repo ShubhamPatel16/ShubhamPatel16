5aa3b1f1899ed9a82b365a6a48403176cbfa82ed
ShubhamPatel16 <159245078+ShubhamPatel16@users.noreply.github.com>
2024-02-20 09:55:13 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main()
+{ 
+   int n1,n2,sum;
+   std::cout<<"enter two values";
+   std::cin>>n1>>n2;
+   sum=n1+n2;
+   std::cout<<"sum of two values:"<<sum;
+    return 0;
+}
